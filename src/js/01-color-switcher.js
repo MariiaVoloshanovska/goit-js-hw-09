e1f0fcc4b8f184ffb532ebf789a5e915a1b8595b
MariiaVoloshanovska <masterskaiamary@gmail.com>
2023-05-29 21:08:03 +0300

    update task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,15 @@
-
+//  посилання на кнопки
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+// ідентифікатор інтервалу
+let timerId = null;
+// Ф-ція випадкового кольору
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+// Ф-ція призначення кольору для body
+function changeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
