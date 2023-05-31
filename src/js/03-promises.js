58af010c987dde2b30d91a4dc40dceb6a902dd98
MariiaVoloshanovska <masterskaiamary@gmail.com>
2023-05-31 16:29:28 +0300

    updated tasks

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,18 +1,50 @@
 import Notiflix from 'notiflix';
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const ourForm = document.querySelector('.form');
+
+// Додавання події `submit` до форми і прив'язка функції `onSubmit` як обробника події
+ourForm.addEventListener('submit', turnOnOurSubmit);
+
+// Функція, яка створює проміс з випадковими значеннями
+function newPromises(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    // Встановлення таймеру, який викликається після затримки
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+// Функція, яка виконується при подачі форми
+function turnOnOurSubmit(evt) {
+  // Запобігання перезавантаження сторінки
+  evt.preventDefault();
+  // Отримання значень полів форми за допомогою властивості `elements`
+  const { delay, step, amount } = evt.currentTarget.elements;
+
+  // Цикл, який генерує проміси в залежності від значення `amount`
+  for (let i = 0, position = 1; i < amount.value; i += 1, position += 1) {
+    // Обчислення загальної затримки для кожного промісу
+    const delayAll = Number(delay.value) + step.value * i;
+    // Виклик функції `createPromise` для створення промісу з позицією та затримкою
+    newPromises(position, delayAll)
+      .then(({ position, delay }) => {
+        // Якщо проміс виконується, виводиться повідомлення про успіх
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        // Якщо проміс не виконується - помилкa
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+}
