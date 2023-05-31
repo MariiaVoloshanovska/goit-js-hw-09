a70d9e8c323c7e49999d42907950557dff38fc69
MariiaVoloshanovska <122610336+MariiaVoloshanovska@users.noreply.github.com>
2023-05-31 19:16:12 +0000

    Deploying to gh-pages from @ MariiaVoloshanovska/goit-js-hw-09@76507aa2c4020631febb88a5026ae5f7512c18ab 🚀

---
+++ 01-color-switcher.f9ef65ec.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval((function(){a()}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.f9ef65ec.js.map
